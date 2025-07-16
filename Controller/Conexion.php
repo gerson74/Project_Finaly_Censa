2407a903f89c719d6358f0e4975a84b2ab6c9930
Ingrid Largo <gersonlargo74@gmail.com>
2025-07-15 20:52:28 -0500

    Mi primier push

---
+++ Controller/Conexion.php
@@ -0,0 +1,26 @@
+<?php
+class Conexion {
+    public function _construct(){}
+    public function _destruct(){}
+
+    public function conectar(){
+        include_once "Configuracion.php";
+        $con=null;
+        try{
+       $con=new PDO("mysql:localhost=".HOST.":81;dbname=".DB.";charset=utf8",USUARIO,CLAVE);
+				$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);	
+        } 
+        
+        catch(PDOException $ex ){
+				echo "BASE DE DATOS INCORRECTA".$ex->getMessage();
+               
+			}
+
+        return $con;
+
+    }
+
+}
+
+
+?> 
---
+++ Controller/Configuracion.php
@@ -0,0 +1,6 @@
+<?php
+define("HOST","localhost"); // este es el Hosting
+define("DB","bdcensa");
+define("USUARIO","root");
+define("CLAVE","");
+?>
